88405b43d8611cab58f14950a2d75d8ddefdc70f
felixdusengimana <phelixdusengimana@gmail.com>
2024-07-04 11:54:22 +0200

    top ten

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
-"""DOC"""
+"""
+function that queries the 'Reddit API' and returns the number of subscribers
+"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """DOC"""
-    reddit_url = "https://www.reddit.com/r/{}/about.json" \
-        .format(subreddit)
+    """
+    number of subscribers
+    """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}  # avoid Too Many Requests error
 
-    header = {'User-agent': 'Mozilla/5.0'}
-    response = requests.get(reddit_url,
-                            headers=header
-                            )
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
-        data = response.json()['data']
-        subs = data['subscribers']
-        return subs
-    return 0
+        data = response.json()
+        return data['data']['subscribers']
+    else:
+        return 0
